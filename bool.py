59f243401c6882083b031087996cc229a307b902
deviprajwala <deviprajwala3@gmail.com>
2021-05-11 15:41:30 +0530

    Done!!!

--- bool.py
+++ bool.py
@@ -1,93 +1,196 @@
 #implementation of boolean model for document retrival
 
 import pandas
+#module to read the contents of the file from a csv file
+
+from contextlib import redirect_stdout
+#module to redirect the output to a text file
+
 terms = []
+#list to store the terms present in the documents
+
 keys = []
+#list to store the names of the documents 
+
 vec_dic = {}
+#dictionary to store the name of the document and the boolean vector as list
+
 dicti = {}
+#dictionary to store the name of the document and the terms present in it as a vector
+
 dummy_list = []
-    
-def filter(documents,rows,cols):
-    for i in range(rows):
-        for j in range(cols):
-            if(j == 0):
-                keys.append(documents.loc[i].iat[j])
+#list for performing some operations and clearing them  
+   
+def filter( documents, rows, cols ):
+    '''function to read and separate the name of the documents and the terms present in it to a separate list  from the data frame and also create a dictionary which 
+    has the name of the document as key and the terms present in it as the list of strings  which is the value of the key'''
+    for i in range( rows ):
+        for j in range( cols ):
+            #traversal through the data frame
+
+            if( j == 0 ):
+                #first column has the name of the document in the csv file
+                keys.append( documents.loc[i].iat[j] )
             else:
-                dummy_list.append(documents.loc[i].iat[j])
+                dummy_list.append( documents.loc[i].iat[j] )
+                #dummy list to update the terms in the dictionary
+
                 if documents.loc[i].iat[j] not in terms:
-                    terms.append(documents.loc[i].iat[j])
+                    #add the terms to the list if it is not present else continue
+                    terms.append( documents.loc[i].iat[j] )
                 
         copy = dummy_list.copy()
-        dicti.update({documents.loc[i].iat[0]:copy})
-        dummy_list.clear()
+        #copying the the dummy list to a different list
 
-def bool_representation(dicti,rows,cols):
+        dicti.update( { documents.loc[i].iat[0]:copy } )
+        #adding the key value pair to a dictionary
+
+        dummy_list.clear()
+        #clearing the dummy list
+
+def bool_representation( dicti, rows, cols ):
+    '''In this fuction we get a boolean representation of the terms present in the documents in the form of lists, later we create a dictionary which
+    contains the the name of the documents as key and value as the list of boolean values representing the terms present in the document'''
+    
     terms.sort()
-    for i in (dicti):
+    #we sort the elements in the alphabetical order for the convience, the order of the term does not make any difference
+
+    for i in ( dicti ):
+        #for every document in the dictionary we check for each string present in the list
+
         for j in terms:
+            #if the string is present in the list we appnd 1 else we append 0
+
             if j in dicti[i]:
-                dummy_list.append(1)
+                dummy_list.append( 1 )
             else:
-                dummy_list.append(0)
+                dummy_list.append( 0 )
+            #appending 1 or 0 for obtaining the boolean representation
+
         copy = dummy_list.copy()
-        vec_dic.update({i:copy})
+         #copying the the dummy list to a different list
+
+        vec_dic.update( { i:copy } )
+        #adding the key value pair to a dictionary
+
         dummy_list.clear()
+        #clearing the dummy list
 
-def query_vector(query):
+def query_vector( query ):
+    '''In this function we represent the query in the form of boolean vector'''
+
     qvect = []
-    for i in terms:
-        if i in query:
-            qvect.append(1)
-        else:
-            qvect.append(0)
-    return qvect        
+    #query vector which is returned at the end of the function
 
-def prediction(q_vect):
+    for i in terms:
+        #if the word present in the list of terms is also present in the query then append 1 else append 0
+
+        if i in query:
+            qvect.append( 1 )
+        else:
+            qvect.append( 0 )
+
+    return qvect
+    #return the query vector which is obtained in the boolean form        
+
+def prediction( q_vect ):
+    '''In this function we make the prediction regarding which document is related to the given query by performing the boolean operations'''
+
     dictionary = {}
     listi = []
     count = 0
+    #initialisation of the dictionary , list and a variable which is further required for performing the compuation
+    
+    term_len = len( terms )
+    #number of terms present in the term list
+
     for i in vec_dic:
-        #print(vec_dic[i]+q_vect)
-        term_len = len(terms)
+        #for every document in the dictionary containing the terms present in it the form of boolean vector
 
-        for t in range(term_len):
-            if(q_vect[t] == vec_dic[i][t]):
-                count+=1
-        dictionary.update({i:count})
+        for t in range( term_len ):
+            if( q_vect[t] == vec_dic[i][t] ):
+                #if the words present in the query is also present in the document or if the words present in the query is also absent in the document 
+                #increase the value of count variable by one 
+                #the condition in which words present in document and absent in query , present in query and absent in document is not considered
+                count += 1
+
+        dictionary.update( { i:count }  )
+        #dictionary updation here the name of the document is the key and the count variable computed earlier is the value
+
         count = 0
-    print(dictionary)
+        #reinitialisaion of count variable to 0
+
     for i in dictionary:
-        listi.append(dictionary[i])
-    listi = sorted(listi,reverse = True)
+        listi.append( dictionary[i] )
+        #here we append the count value to list
+
+    listi = sorted( listi, reverse = True )
+    #we sort the list in the decending order which is needed to rank the documents according to the relevance
+
     ans = ' '
+    #variable to store the name of the document which is most relevant
 
-    print("ranking of the documents")
-    for count,i in enumerate(listi):
-        key = check(dictionary,i)
-        if count == 0:
-            ans = key
-        print(key ,"rank is ",count+1)
-        dictionary.pop(key)
+    with open('output.txt', 'w') as f:
+        with redirect_stdout( f ):
+            #to redirect the output to a text file
+         
+            print( "ranking of the documents" )
 
-    print(ans, "is the most relevant document for the given query")
+            for count,i in enumerate( listi ):
+                key = check( dictionary, i )
+                #Function call to get the key when the value is known
+                if count == 0:
+                    ans = key
+                    #to store the name of the document which is most relevant
 
-def check(dictionary, val):
+                print( key, "rank is", count+1 )
+                #print the name of the document along with its rank
+
+                dictionary.pop( key )
+                #remove the key from the dictionary after printing
+
+            print( ans, "is the most relevant document for the given query" )
+            #to print the name of the document which is most relevant
+
+def check( dictionary, val ):
+    '''Function to return the key when the value is known'''
+
     for key, value in dictionary.items():
-        if(val == value):
+        if( val == value ):
+            #if the given value is same as the value present in the dictionary return the key
+
             return key
 
 def main():
     documents = pandas.read_csv(r'documents.csv')
-    rows = len(documents)
-    cols = len(documents.columns) 
-    filter(documents,rows,cols)
-    #print(dicti)        
-    bool_representation(dicti,rows,cols)
-    print("Enter query")
+    #to read the data from the csv file as a dataframe
+
+    rows = len( documents )
+    #to get the number of rows
+
+    cols = len( documents.columns ) 
+    #to get the number of columns
+
+    filter( documents, rows, cols )
+    #function call to read and separate the name of the documents and the terms present in it to a separate list  from the data frame and also create a dictionary which 
+    #has the name of the document as key and the terms present in it as the list of strings  which is the value of the key
+           
+    bool_representation( dicti, rows, cols )
+    #In this fuction we get a boolean representation of the terms present in the documents in the form of lists, later we create a dictionary which
+    #contains the the name of the documents as key and value as the list of boolean values representing the terms present in the document
+    
+    print( "Enter query" )
     query = input()
+    #to get the query input from the user, the below input is given for obtaining the output as in output.txt file
     #hockey is a national sport
+
     query=query.split(' ')
-    q_vect=query_vector(query)
-    prediction(q_vect)
+    #spliting the query as a list of strings
+
+    q_vect=query_vector( query )
+    #function call to represent the query in the form of boolean vector
+
+    prediction( q_vect )
+    #Function call to make the prediction regarding which document is related to the given query by performing the boolean operations
 
 main()
